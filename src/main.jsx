c9b940af212cede9a8870f3540682c3b037813a2
Dmytro Lobanov <dspwww@gmail.com>
2025-05-11 16:47:52 +0100

    init: Add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/checkout",
+    element: <h1>Checkout page</h1>
+  }
+]);
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
