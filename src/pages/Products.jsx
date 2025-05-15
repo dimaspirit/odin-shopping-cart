3008d67fe3f2ab8fcc5722994e1da7e462b62b58
Dmytro Lobanov <dspwww@gmail.com>
2025-05-15 17:52:35 +0100

    feat: Add daisyui & style a product / list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,29 @@
 import { useEffect, useState } from "react";
 
+function Product({product}) {
+  return (
+    <div className="pt-4 card bg-base-100 shadow-sm">
+      <figure>
+        <img
+          className="w-full h-80 object-contain"
+          src={product.image}
+          alt={product.title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {product.title}
+        </h2>
+        <p className="line-clamp-3 md:line-clamp-4">{product.description}</p>
+        <div className="mt-2 card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const CATEGORY = 'men\'s clothing';
+
 function Products () {
   const [products, setProducts] = useState([]);
 
@@ -7,13 +31,17 @@ function Products () {
 
     fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
-      .then(data => setProducts(data));
+      .then(data => setProducts(data.filter(item => item.category === CATEGORY)));
   }, []);
 
   return (
     <>
-      <h1>Products page</h1>
-      {products.map(product => <h3 key={product.id}>{product.title}</h3>)}
+      <h1 className="text-2xl font-bold text-gray-900">Products page</h1>
+      <div className="mt-6 gap-12 md:grid md:grid-cols-2 lg:grid-cols-3 lg:space-y-0 lg:gap-x-6">
+        {products.map(product => (
+            <Product key={product.id} product={product} />
+        ))}
+      </div>
     </>
   )
 }
