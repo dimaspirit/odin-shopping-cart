98594edfde42d382f97415b87069c6f63063e4bf
Dmytro Lobanov <dspwww@gmail.com>
2025-05-15 17:01:39 +0100

    feat: Get Products from API & render them

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
+
 function Products () {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
   return (
-    <h1>Products page</h1>
+    <>
+      <h1>Products page</h1>
+      {products.map(product => <h3 key={product.id}>{product.title}</h3>)}
+    </>
   )
 }
 
