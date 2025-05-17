084a2bb8118e021bc98ed1c3e197e535ccd41e8f
Dmytro Lobanov <dspwww@gmail.com>
2025-05-17 20:15:11 +0100

    feat: Add Qty functionality

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,12 @@
-function CartItem({product, onRemove}) {
+function CartItem({product, onRemove, onChangeQty}) {
+  const handleChangeQty = (event) => {
+    const value = event.target.value;
+    console.log('handleChangeQty', value);
+
+    onChangeQty(product.id, +value);
+  }
+
+
   return (
     <li className="flex py-6">
       <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
@@ -16,7 +24,20 @@ function CartItem({product, onRemove}) {
           <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
         </div>
         <div className="flex flex-1 items-end justify-between text-sm">
-          <p className="text-gray-500">Qty 1</p>
+          <div>
+            <input
+              type="number"
+              className="input input-sm validator"
+              required
+              value={product.qty}
+              placeholder="Qty"
+              min="1"
+              max="99"
+              title="Must be between be 1 to 99"
+              onChange={handleChangeQty}
+            />
+            <p className="validator-hint">Must be between be 1 to 99</p>
+          </div>
 
           <div className="flex">
             <button className="btn btn-ghost" onClick={() => onRemove(product.id)}>Remove</button>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -22,6 +22,22 @@ function Cart () {
     setCart(cart.filter(item => item.id !== id));
   }
 
+  const changeQty = (id, qty) => {
+    const cartUpdated = cart.map(item => {
+      if(item.id === id) {
+        return {
+          ...item,
+          qty,
+        }
+      }
+
+      return item;
+    });
+    console.log('cartUpdated', cartUpdated);
+
+    setCart(cartUpdated);
+  }
+
   return (
     <>
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -30,13 +46,13 @@ function Cart () {
 
       <div className="flow-root sm:mx-auto sm:w-full sm:max-w-sm mt-8">
         <ul role="list" className="-my-6 divide-y divide-gray-200">
-          {productFullInfo.map(p => <CartItem key={p.id} product={p} onRemove={handleRemoveItem} />) }
+          {productFullInfo.map(p => <CartItem key={p.id} product={p} onRemove={handleRemoveItem} onChangeQty={changeQty} />) }
         </ul>
 
         <div className="border-t border-gray-200 px-4 py-6 mt-8 sm:px-6">
           <div className="flex justify-between text-base font-medium text-gray-900">
             <p>Subtotal</p>
-            <p>${total}</p>
+            <p>${total.toFixed(2)}</p>
           </div>
 
           <div className="mt-6">
